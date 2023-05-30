3bf1c5ac4ac438dbda2f523d412ad791f67c0e82
Mutindi Hope <msndindi@outlook.com>
2023-05-30 08:00:36 +0300

    Completed Assignment

--- lib/debug.py
+++ lib/debug.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 
+
 from config import CONN, CURSOR
 from song import Song
 
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+
+
+# hello = Song("Hello", "25")
+# hello.save()
+# print(hello)
+
+# despacito = Song("Despacito", "Vida")
+# despacito.save()
+
+# songs = CURSOR.execute('SELECT * FROM songs')
+# [row for row in songs]
